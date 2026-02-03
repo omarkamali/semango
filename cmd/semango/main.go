187e73ecbfcf4dbe13bb38c2337d7e274f462aa8
Omar <semango@omarkama.li>
2026-02-03 23:25:26 +0000

    Remove extra line

--- cmd/semango/main.go
+++ cmd/semango/main.go
@@ -354,7 +354,6 @@ func init() {
 	rootCmd.AddCommand(searchCmd)
 	rootCmd.AddCommand(serverCmd)
 	rootCmd.AddCommand(versionCmd)
-	rootCmd.AddCommand(installCmd)
 	initCmd.Flags().StringP("file", "f", config.DefaultConfigPath, "Path to write the configuration file")
 	rootCmd.PersistentFlags().StringP("config", "c", config.DefaultConfigPath, "Path to the configuration file")
 	rootCmd.PersistentFlags().BoolP("verbose", "v", false, "Enable verbose logging")
